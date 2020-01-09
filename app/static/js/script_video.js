47f86c961137562381ead19ccca796d76bd2438d
jonathanvanschenck <44685047+jonathanvanschenck@users.noreply.github.com>
2020-01-09 04:26:17 +0000

    Add files via upload

--- app/static/js/script_video.js
+++ app/static/js/script_video.js
@@ -1,1 +1,50 @@
+console.log('Works');
 
+_bufferConvert = function(buffer) {
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength();
+    for (var i = 0; i < len; i ++) {
+        binary += String.fromCharCode(bytes[i])
+    }
+    return window.btoa(binary);
+};
+
+
+var socket = io();
+
+socket.on('connect', function() {
+    socket.emit('echo', {data: 'Connected at js'});
+});
+
+socket.on('log_msg', function(msg) {
+    console.log('Logging' + msg.data);
+    //var newmsg = $("<li/>");
+    //newmsg.text(msg.data);
+    //$("#log").append(newmsg);
+});
+
+socket.on('update_img', function(msg) {
+    $("#img-container").attr("src","data:image/jpeg;base64," + msg.data)
+});
+
+onload = function() {
+    get_img_start();
+};
+
+get_img_one = function() {
+    socket.emit('get_img');
+};
+
+var runID = 0;
+get_img_start = function() {
+    //console.log("starting")
+    runID = window.setInterval(function(){
+      socket.emit('get_img');
+    }, 100);
+    //console.log("set runID: " + runID)
+};
+get_img_stop = function() {
+    //console.log("stopping")
+    clearInterval(runID);
+};
